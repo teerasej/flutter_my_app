cf6a3140e93b76cf5cd629b463729b5d085e17e5
Teerasej <teerasej@gmail.com>
2024-09-17 13:35:53 +0700

    first commit

---
+++ lib/hello_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Hello extends StatelessWidget {
+  const Hello({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Icon(Icons.delete),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/hello_widget.dart';
+
+main() {
+  print('hello');
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var app = MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: false,
+        primarySwatch: Colors.orange,
+      ),
+      title: 'my app',
+      home: CounterArea(),
+    );
+
+    return app;
+  }
+}
+
+class CounterAreaStatic extends StatelessWidget {
+  const CounterAreaStatic({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class CounterArea extends StatefulWidget {
+  const CounterArea({super.key});
+
+  @override
+  State<CounterArea> createState() => _CounterAreaState();
+}
+
+class _CounterAreaState extends State<CounterArea> {
+  var _count = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("home"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "count: $_count",
+              style: TextStyle(fontSize: 60.5),
+            ),
+            Text("กดปุ่มเพื่อเพิ่มตัวนับ")
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _count += 1;
+          print(_count);
+          setState(() {});
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
